5994299ffedbf5b3af628e1bc192577ca8ff1591
alumno Secundaria <al3201@cedi.local>
2018-10-15 09:26:45 -0500

    Agregar boton y pagina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  boc = BocinaPage;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  clickBocina(){
+    this.navCtrl.push(this.boc);
+  }
 }
